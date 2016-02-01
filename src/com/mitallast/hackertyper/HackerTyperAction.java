8a4cffa78823af26ab6743913cb53c3b2ff2e63f
Alexey Korchevskiy <mitallast@gmail.com>
2016-02-01 13:29:37 +0300

    improve java & php file types support

--- src/com/mitallast/hackertyper/HackerTyperAction.java
+++ src/com/mitallast/hackertyper/HackerTyperAction.java
@@ -2,33 +2,27 @@ package com.mitallast.hackertyper;
 
 import com.intellij.codeInsight.template.impl.editorActions.TypedActionHandlerBase;
 import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.actionSystem.TypedActionHandler;
+import com.intellij.openapi.fileEditor.FileDocumentManager;
+import com.intellij.openapi.vfs.VirtualFile;
 import sun.util.logging.PlatformLogger;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class HackerTyperAction extends TypedActionHandlerBase {
     private static final PlatformLogger logger = PlatformLogger.getLogger("com.mitallast.hackertyper.HackerTyperAction");
 
-    private static final String sourceCode;
-    private static final AtomicInteger sourcePointer = new AtomicInteger();
+    private static final String javaSourceCode;
+    private static final String phpSourceCode;
 
     static {
-        String fileContent = "default content";
-        try (InputStream resource = HackerTyperAction.class.getResourceAsStream("source_code.txt")) {
-            fileContent = new BufferedReader(new InputStreamReader(resource))
-                .lines()
-                .reduce((s, s2) -> s + "\n" + s2)
-                .get();
-        } catch (Throwable e) {
-            logger.warning("error load resource", e);
-        } finally {
-            sourceCode = fileContent;
-        }
+        javaSourceCode = loadSource("java_source_code.txt");
+        phpSourceCode = loadSource("php_source_code.txt");
     }
 
     public HackerTyperAction(TypedActionHandler typedActionHandler) {
@@ -37,13 +31,48 @@ public class HackerTyperAction extends TypedActionHandlerBase {
 
     @Override
     public void execute(Editor editor, char c, DataContext dataContext) {
-        logger.info("editor " + editor + " char " + c + " data context " + dataContext);
+        final Document document = editor.getDocument();
+        final VirtualFile virtualFile = FileDocumentManager.getInstance().getFile(document);
+        if (virtualFile != null) {
+            logger.info("virtual file type: " + virtualFile.getFileType().getName());
 
-        int next = sourcePointer.getAndIncrement() % sourceCode.length();
-        c = sourceCode.charAt(next);
+            int offset = editor.getCaretModel().getOffset();
+
+            switch (virtualFile.getFileType().getName()) {
+                case "JAVA":
+                    c = nextJavaSymbol(offset);
+                    break;
+                case "PHP":
+                    c = nextPhpSymbol(offset);
+                    break;
+            }
+        }
 
         if (myOriginalHandler != null) {
             myOriginalHandler.execute(editor, c, dataContext);
         }
+    }
+
+    private char nextJavaSymbol(int offset) {
+        int next = offset % javaSourceCode.length();
+        return javaSourceCode.charAt(next);
+    }
+
+    private char nextPhpSymbol(int offset) {
+        int next = offset % phpSourceCode.length();
+        return phpSourceCode.charAt(next);
+    }
+
+    private static String loadSource(String path) {
+        String fileContent = "default content";
+        try (InputStream resource = HackerTyperAction.class.getResourceAsStream(path)) {
+            fileContent = new BufferedReader(new InputStreamReader(resource))
+                .lines()
+                .reduce((s, s2) -> s + "\n" + s2)
+                .get();
+        } catch (IOException e) {
+            logger.warning("error load resource", e);
+        }
+        return fileContent;
     }
 }
